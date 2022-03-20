e6ddc7514a60584c9784c4112d1b512926b4f28a
ProgramSmart <69406870+ProgramSmarter@users.noreply.github.com>
2022-03-20 16:57:10 -0700

    Create coolvarstuff.java
    siiii

---
+++ coolvarstuff.java
@@ -0,0 +1,10 @@
+public class coolvarstuff {
+  public static void main(String[] args) {
+    String hi = " Hello!";
+    int x = 2;
+    int y = 6;
+    float Myfloat = int y;
+    System.out.println(x + Myfloat + hi);
+	
+  }
+}
