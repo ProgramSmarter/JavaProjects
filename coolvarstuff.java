968b3c893ca097ab78ea3afc8084c963bd71ed80
ProgramSmart <69406870+ProgramSmarter@users.noreply.github.com>
2022-03-21 15:42:34 -0700

    Update coolvarstuff.java

--- coolvarstuff.java
+++ coolvarstuff.java
@@ -2,9 +2,29 @@ public class coolvarstuff {
   public static void main(String[] args) {
     String hi = " Hello!";
     int x = 2;
+    x = 4;
+    x = 6;
     int y = 6;
-    float Myfloat = int y;
+    if (x >= y) {
+      System.out.println("x is greater than y");
+    }
+    else {
+      System.out.println("sus");
+    }
+    float Myfloat = y;
     System.out.println(x + Myfloat + hi);
+    x = (int) Myfloat;
+    System.out.println(x);
+    String lengthoftext = "The length of this string is ";
+    System.out.println(lengthoftext + lengthoftext.length() + " characters");
+    System.out.println(lengthoftext.indexOf("t"));
+    System.out.println(Math.min(5, 7));
+    System.out.println(Math.sqrt(128));
+    int sus = 4;
+    int suss = 5;
+    System.out.println((boolean)(sus==suss));
+
+
 	
   }
 }
